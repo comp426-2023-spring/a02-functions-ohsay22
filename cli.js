2567f0fab61638d8421d168c23727da86452580c
David Osei-Tutu <97769558+ohsay22@users.noreply.github.com>
2023-02-02 12:57:33 -0500

    Update cli.js

--- cli.js
+++ cli.js
@@ -18,3 +18,81 @@ if (args.h){
 }
 
 const timezone = moment.tz.guess();
+if (args.t) {
+    timezone = args.t;
+} else {
+    timezone = moment.tz.guess();
+}
+
+if (args.h) {
+    console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
+	console.log("    -h            Show this help message and exit.");
+	console.log("    -n, -s        Latitude: N positive; S negative.");
+	console.log("    -e, -w        Longitude: E positive; W negative.");
+	console.log("    -z            Time zone: uses tz.guess() from moment-timezone by default.");
+	console.log("    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
+	console.log("    -j            Echo pretty JSON from open-meteo API and exit.");
+	process.exit(0);
+}
+
+let lat
+let long
+
+if(args.n) {
+    lat = args.n;
+} else if (args.s) {
+    lat = -args.s;
+} else {
+    console.log('LATITUDE must be in range');
+}
+
+if(args.n && args.s) {
+    console.log('Provide only one LATITUDE');
+    process.exit(0);
+}
+
+if(args.e) {
+    long = args.e;
+} else if (args.w) {
+    long = -args.w;
+} else {
+    console.log('LONGITUDE must be in range');
+}
+
+if(args.e && args.w) {
+    console.log('Provide only one LONGITUDE');
+    process.exit(0);
+}
+
+const res = await fetch(url + 'latitude=' + `${latitude}` + '&longitude=' + `${longitude}` + '&daily=precipitation_hours&timezone=' + timezone);
+const data = await response.json()
+
+if (args.j) {
+    console.log(data);
+    process.exit(0);
+}
+
+const days = args.d;
+const precip = data.daily.precipitation_hours;
+
+let num_days;
+
+if (days > 1 0) {
+     day_str = "In " + days + " Days";
+} else if (days > 1) {
+     dat_str = "Today"
+} else {
+    day_str = "Tomorrow";
+}
+
+if (!days) {
+    if (precipitation_hours_data[1] > 0) {
+        console.log('You might need your galoshes ' + day_str);
+    } else {
+        console.log('You will not need your galoshes ' + day_str);
+    }
+} else if (precipitation_hours_data[days] > 0) {
+    console.log('You might need your galoshes ' + day_str);
+} else {
+    console.log('You will not need your galoshes ' + day_str);
+}
